ef4e557f3e5c5ec6745d8f5b57a919419963464d
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2020-01-28 04:37:00 +0800

    Add key pair

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -16,7 +17,7 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		instanceType, _ := cmd.Flags().GetString("instance-type")
 		region, _ := cmd.Flags().GetString("region")
-		isSpot, _ := cmd.Flags().GetBool("spot")
+		spot, _ := cmd.Flags().GetBool("spot")
 
 		sess, err := session.NewSession(&aws.Config{
 			Region:      aws.String(region),
@@ -27,12 +28,20 @@ var rootCmd = &cobra.Command{
 		}
 
 		svc := ec2.New(sess)
+		result, err := svc.CreateKeyPair(&ec2.CreateKeyPairInput{KeyName: aws.String("run-on-ec2")})
+		if err == nil {
+			keyFile, _ := os.Create("run-on-ec2.pem")
+			keyFile.WriteString(*result.KeyMaterial)
+			keyFile.Sync()
+			keyFile.Close()
+		}
 
-		if isSpot {
+		if spot {
 			requestResult, err := svc.RequestSpotInstances(&ec2.RequestSpotInstancesInput{
 				LaunchSpecification: &ec2.RequestSpotLaunchSpecification{
 					ImageId:      aws.String("ami-06e882db7f01fad97"),
 					InstanceType: aws.String(instanceType),
+					KeyName:      aws.String("run-on-ec2"),
 				},
 			})
 
@@ -48,6 +57,7 @@ var rootCmd = &cobra.Command{
 				InstanceType: aws.String(instanceType),
 				MinCount:     aws.Int64(1),
 				MaxCount:     aws.Int64(1),
+				KeyName:      aws.String("run-on-ec2"),
 			})
 
 			if err != nil {
@@ -68,5 +78,6 @@ func Execute() error {
 func init() {
 	rootCmd.Flags().StringP("instance-type", "i", "t2.micro", "ec2 instance type")
 	rootCmd.Flags().StringP("region", "r", "eu-central-1", "aws session region")
-	rootCmd.Flags().BoolP("spot", "s", true, "use spot instances")
+	rootCmd.Flags().BoolP("spot", "s", true, "request spot instances")
+	rootCmd.Flags().IntP("volume", "v", 1, "volume attached in GiB")
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,7 @@ go 1.13
 
 require (
 	github.com/aws/aws-sdk-go v1.28.9
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/spf13/cobra v0.0.5
-	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	github.com/torusresearch/torus-common v0.0.0-20200115030411-ff23a0c24707
 	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
