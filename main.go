978bb79af1a560b9f3801c0098810d36546b7046
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2020-02-01 20:55:26 +0800

    Set log level to debug

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"github.com/cheran-senthil/run-on-ec2/cmd"
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
 
 func main() {
 	cmd.Execute()
